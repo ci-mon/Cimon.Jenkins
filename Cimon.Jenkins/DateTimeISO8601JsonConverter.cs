8125bc3263ad7988ae32fa59a47d43993e511ea4
Vadym Artemchuk <v.artemchuk@creatio.com>
2024-03-26 00:33:22 +0200

    dates parsing fix

--- Cimon.Jenkins/DateTimeISO8601JsonConverter.cs
+++ Cimon.Jenkins/DateTimeISO8601JsonConverter.cs
@@ -7,9 +7,21 @@ namespace Cimon.Jenkins;
 
 class DateTimeIso8601JsonConverter : JsonConverter<DateTime>
 {
-	private const string Format = "yyyy-MM-dd HH:mm:ss zzz";
+	private const string Format = "yyyy-MM-ddTHH:mm:ss.ffffffZ";
+	private const string OffsetFormat = "yyyy-MM-dd HH:mm:ss zzz";//2024-03-25 18:08:47 +0200
+
 	public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-		return DateTime.ParseExact(reader.GetString() ?? string.Empty, Format, CultureInfo.InvariantCulture);
+		var value = reader.GetString();
+		if (string.IsNullOrWhiteSpace(value))
+			return default;
+		if (DateTimeOffset.TryParseExact(value, OffsetFormat, CultureInfo.InvariantCulture,
+				DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out var dateOffset)) {
+			return dateOffset.UtcDateTime;
+		}
+		return DateTime.TryParseExact(value, Format, CultureInfo.InvariantCulture,
+			DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out DateTime date)
+			? date.ToUniversalTime()
+			: default;
 	}
 
 	public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) {
